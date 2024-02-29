ab7f498773d2d9f58d389ec40c9680786c829d95
arkaramadhan <arkaramadhan1933@gmail.com>
2024-02-29 21:04:03 +0700

    update

--- edit.php
+++ edit.php
@@ -11,6 +11,11 @@
 	<link href="../css/styles.css" rel="stylesheet" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+	<style>
+		label{
+			font-weight: 700;
+		}
+	</style>
 </head>
 
 <div class="formbold-main-wrapper">
--- tambah2.php
+++ tambah2.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css">
     <title>Document</title>
+    <style>
+        label{
+            font-weight:700;
+        }
+    </style>
 </head>
 
 <body>
