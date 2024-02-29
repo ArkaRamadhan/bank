994d7d2a1b35b7b758d276c217e18a79240bcb28
arkaramadhan <arkaramadhan1933@gmail.com>
2024-02-29 21:50:07 +0700

    sql

--- db_bank.sql
+++ db_bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2024 at 07:15 AM
+-- Generation Time: Feb 29, 2024 at 03:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -59,7 +59,6 @@ CREATE TABLE `bank` (
   `keterangan` text NOT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_akhir` date NOT NULL,
-  `bisnis` varchar(255) NOT NULL,
   `coorporate_id` varchar(255) NOT NULL,
   `coorporate` varchar(255) NOT NULL,
   `id` varchar(255) NOT NULL,
@@ -73,17 +72,22 @@ CREATE TABLE `bank` (
 -- Dumping data for table `bank`
 --
 
-INSERT INTO `bank` (`id_ib`, `username`, `nama`, `bank`, `cabang`, `no_rekening`, `nama_ibu`, `no_hp`, `email`, `password`, `user_ib`, `pin_ib`, `kode_akses`, `password_transaksi`, `pin_mb`, `pin_atm`, `serial_key_number`, `pin_skn`, `jenis_atm`, `no_kartu_atm`, `cvv`, `masa_berlaku_atm`, `valid_simcard`, `status`, `batch`, `user_my_bca`, `password_my_bca`, `pin_transaksi`, `keterangan`, `tanggal_mulai`, `tanggal_akhir`, `bisnis`, `coorporate_id`, `coorporate`, `id`, `foto_ktp`, `foto_kartu_atm`, `foto_kk`, `foto_buku_tabungan`) VALUES
-(20, 'tolil', 'LIEUR', 'LIEUR', 'TEUING', '123 LIEUR EUY', 'POHO', '022231', 'qwert@gmail.com', 'qwerty', '1213', 'sdawd2', 'asd', '123', 'sdawd2', '1232', 'sdqw2', 'asdw', '123daas', 'sad2', 'jhws', '2024-02-26', '2024-02-26', 'Blockir', '1 // KOCAK', 'ahjs', 'kjdabsdkj', 'ajbds', 'daksdbjw', '2024-02-26', '2024-02-26', 'qjksdn', 'ajsbdkansmdb', 'sdbk', 'qkdbj', '', '', '', ''),
-(21, '', 'quwihe', 'iuhdadjh', 'sjhadjKH', 'sjdhfq', 'ajdh', 'qwes', 'awejhas@gmail.com', 'jshd', 'uiashd', 'diaushd', 'dauishd', 'duiashd', 'diaushd', 'wduiahsd', 'idSHASD', 'AUISHD', 'asuidh', 'audhis', 'sdf', '2024-03-05', '2024-03-09', 'Closed', 'igdajkbs', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '1496216378_ktp.jpeg', '1496216378_kartu_atm.jpeg', '1496216378_kk.jpeg', '1496216378_buku_tabungan.jpeg'),
-(24, 'tolil', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '06788444_ktp.jpg', '06788443_kartu_atm.jpeg', '06788445_kk.jpg', '06788446_buku_tabungan.jpg'),
-(25, 'tolil', 'asdasd', 'dasdas', 'dasd', 'asd', 'das', 'dasd', 'dadasas@gmail.com', 'dasd', 'das', 'dasd', 'das', 'sdsa', 'dasd', 'asdasd', 'asdd', 'dasd', 'dasdds', 'dasda', 'sasadsdas', '2024-03-01', '2024-02-27', '', 'asdasd', '2dasd', 'asdq2', 'dsad', 'dasd', '2024-02-27', '2024-02-27', 'asd', 'wdasd', 'dsad', 'asdsad', '', '', '', ''),
-(26, 'ujang', '123asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd@gmail.com', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', '0000-00-00', '0000-00-00', '', 'asd', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '', ''),
-(27, 'ujang', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe@gmail.com', 'eqw', 'qwe', 'qwe', 'qwe', 'qew', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', '2024-02-23', '2024-02-03', '', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', '2024-02-15', '2024-02-09', 'qwe', 'qwe', 'qwe', 'qwe', '', '', '', ''),
-(28, 'ujang', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'active', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '', ''),
-(29, 'ujang', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'blockir', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '', ''),
-(30, 'jamal', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'closed', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '', ''),
-(32, 'kocak', 'Arkha', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'active', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '', '');
+INSERT INTO `bank` (`id_ib`, `username`, `nama`, `bank`, `cabang`, `no_rekening`, `nama_ibu`, `no_hp`, `email`, `password`, `user_ib`, `pin_ib`, `kode_akses`, `password_transaksi`, `pin_mb`, `pin_atm`, `serial_key_number`, `pin_skn`, `jenis_atm`, `no_kartu_atm`, `cvv`, `masa_berlaku_atm`, `valid_simcard`, `status`, `batch`, `user_my_bca`, `password_my_bca`, `pin_transaksi`, `keterangan`, `tanggal_mulai`, `tanggal_akhir`, `coorporate_id`, `coorporate`, `id`, `foto_ktp`, `foto_kartu_atm`, `foto_kk`, `foto_buku_tabungan`) VALUES
+(20, 'tolil', 'LIEUR', 'LIEUR', 'TEUING', '123 LIEUR EUY', 'POHO', '022231', 'qwert@gmail.com', 'qwerty', '1213', 'sdawd2', 'asd', '123', 'sdawd2', '1232', 'sdqw2', 'asdw', '123daas', 'sad2', 'jhws', '2024-02-26', '2024-02-26', 'Blockir', '1 // KOCAK', 'ahjs', 'kjdabsdkj', 'ajbds', 'daksdbjw', '2024-02-26', '2024-02-26', 'ajsbdkansmdb', 'sdbk', 'qkdbj', '', '', '', ''),
+(21, '', 'quwihe', 'iuhdadjh', 'sjhadjKH', 'sjdhfq', 'ajdh', 'qwes', 'awejhas@gmail.com', 'jshd', 'uiashd', 'diaushd', 'dauishd', 'duiashd', 'diaushd', 'wduiahsd', 'idSHASD', 'AUISHD', 'asuidh', 'audhis', 'sdf', '2024-03-05', '2024-03-09', 'Closed', 'igdajkbs', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '1496216378_ktp.jpeg', '1496216378_kartu_atm.jpeg', '1496216378_kk.jpeg', '1496216378_buku_tabungan.jpeg'),
+(24, 'tolil', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '06788444_ktp.jpg', '06788443_kartu_atm.jpeg', '06788445_kk.jpg', '06788446_buku_tabungan.jpg'),
+(25, 'tolil', 'asdasd', 'dasdas', 'dasd', 'asd', 'das', 'dasd', 'dadasas@gmail.com', 'dasd', 'das', 'dasd', 'das', 'sdsa', 'dasd', 'asdasd', 'asdd', 'dasd', 'dasdds', 'dasda', 'sasadsdas', '2024-03-01', '2024-02-27', '', 'asdasd', '2dasd', 'asdq2', 'dsad', 'dasd', '2024-02-27', '2024-02-27', 'wdasd', 'dsad', 'asdsad', '', '', '', ''),
+(26, 'ujang', '123asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd@gmail.com', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', '0000-00-00', '0000-00-00', '', 'asd', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(27, 'ujang', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe@gmail.com', 'eqw', 'qwe', 'qwe', 'qwe', 'qew', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', '2024-02-23', '2024-02-03', '', 'qwe', 'qwe', 'qwe', 'qwe', 'qwe', '2024-02-15', '2024-02-09', 'qwe', 'qwe', 'qwe', '', '', '', ''),
+(28, 'ujang', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'active', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(29, 'ujang', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'blockir', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(30, 'jamal', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'closed', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(32, 'kocak', 'Arkha', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'active', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(33, 'kocak', 'das', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'active', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(34, 'jamal', 'arka', 'BCA', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(35, 'rio', 'arka', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'Active', '1', '', '', '', '', '0000-00-00', '0000-00-00', 'popo', 'popsdbk', 'qkdlpobj', '', '', '', ''),
+(36, 'rio', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'stock', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', ''),
+(37, 'rio', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', '', '', '', '', '', '0000-00-00', '0000-00-00', 'sda', 'asd', 'asd', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -140,8 +144,7 @@ INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
 (6, 'tolil', 'tolil', 'user'),
 (7, 'ujang', 'ujang', 'user'),
 (8, 'pendi', '123', 'user'),
-(9, 'jamal', '123', 'user'),
-(10, 'kocak', 'kocak', 'user');
+(9, 'jamal', '123', 'user');
 
 --
 -- Indexes for dumped tables
@@ -179,7 +182,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bank`
 --
 ALTER TABLE `bank`
-  MODIFY `id_ib` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_ib` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `bisnis`
